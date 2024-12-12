836f659f1aaa6b5f793d2726aea4991a79981632
Navneet Kaur <navkaurneet1@gmail.com>
2024-12-12 17:11:41 -0500

    Update 3_seed_climate_data.sql

--- sql/3_seed_climate_data.sql
+++ sql/3_seed_climate_data.sql
@@ -1,6 +1,6 @@
-INSERT INTO ClimateData (location, record_date, temperature, precipitation) 
+INSERT INTO ClimateData (location, record_date, temperature, precipitation, humidity) 
 VALUES
-  ('New York', '2024-01-01', 5.5, 12.0),
-  ('Los Angeles', '2024-01-01', 18.0, 5.0),
-  ('Chicago', '2024-01-02', -2.0, 20.0),
-  ('Miami', '2024-01-02', 22.0, 10.0);
+  ('New York', '2024-01-01', 5.5, 12.0, 65.0),
+  ('Los Angeles', '2024-01-01', 18.0, 5.0, 60.0),
+  ('Chicago', '2024-01-02', -2.0, 20.0, 80.0),
+  ('Miami', '2024-01-02', 22.0, 10.0, 70.0);
