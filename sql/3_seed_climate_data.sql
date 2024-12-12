e3ef1d2ac26883dc23a49c7a34804d3e1de0ad81
Navneet Kaur <navkaurneet1@gmail.com>
2024-12-12 16:11:31 -0500

    Create 3_seed_climate_data.sql for inserting the data into the db table.

---
+++ sql/3_seed_climate_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO ClimateData (location, record_date, temperature, precipitation) 
+VALUES
+  ('New York', '2024-01-01', 5.5, 12.0),
+  ('Los Angeles', '2024-01-01', 18.0, 5.0),
+  ('Chicago', '2024-01-02', -2.0, 20.0),
+  ('Miami', '2024-01-02', 22.0, 10.0);
